a8b5c7f7f0dc221a994c7fa36dadf2d5647e1a11
Jeremy Winn <jeremyjwinn@gmail.com>
2020-07-07 23:12:56 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,3 +11,11 @@ songs = [
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
 
+def say_hello(name)
+  "Hi #{name}!"
+end
+ 
+puts "Enter your name:"
+users_name = gets.strip
+ 
+puts say_hello(users_name)
